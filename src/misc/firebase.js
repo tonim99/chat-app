97491a9c6ca6e49f8d5732c3762519328c1aa48b
Tonimarie Morris <morris.tonimarie@gmail.com>
2021-04-15 12:37:27 -0400

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+const config = {
+    apiKey: "AIzaSyAbo9noeEIU2jIJi74ivM9pMmeA15_LmWI",
+    authDomain: "chat-web-app-3e22a.firebaseapp.com",
+    projectId: "chat-web-app-3e22a",
+    storageBucket: "chat-web-app-3e22a.appspot.com",
+    messagingSenderId: "760248689054",
+    appId: "1:760248689054:web:ac72b0d8a7d6a04ba1b68f"
+  };
+
+  const app = firebase.initializeApp(config)
+  
